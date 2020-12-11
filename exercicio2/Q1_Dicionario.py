277406eb2d29ffe6d4660a86b0636061c8898d3c
unknown <viniciusfelipe.xd@gmail.com>
2020-12-11 20:54:11 -0300

    atualizado

--- exercicio2/Q1_Dicionario.py
+++ exercicio2/Q1_Dicionario.py
@@ -8,7 +8,8 @@ Onde as chaves sejam os de cada e os valores as alturas.
 altura = dict()
 
 for numAluno in range(10):
-    altura[input('Qual o nome do aluno? ')] = float(input(f'Qual é a altura de ? '))
+    nome = input('Qual o nome do aluno? ')
+    altura[nome] = float(input(f'Qual é a altura de {nome}? '))
 
 print(f'O aluno mais alto é {max(altura,key=altura.get)} com {max(altura.values())}m ')
 print(f'O aluno mais baixo é {min(altura,key=altura.get)} com {min(altura.values())}m ')
